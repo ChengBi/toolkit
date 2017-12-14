adfb8455f78476470d729d6db84ffabf78d6672d
Bi <cheng.bi@nuance.com>
2017-12-14 17:10:09 +0800

    Update

--- src/cnn_lstm.py
+++ src/cnn_lstm.py
@@ -94,14 +94,20 @@ class lstm_layer(layer):
     def __str__(self):
         return self.outputs.__str__()
     
+    
+    
 graph = tf.Graph()
 with graph.as_default():
     
     with tf.name_scope('embedding_lstm'):
 #     if 1 == 1:
+
+        
         layers = {}
-        input_placeholder = tf.placeholder(tf.int32, [1, None])
-        target_placeholder = tf.placeholder(tf.float32, [1, 134])
+        input_placeholder = tf.placeholder(tf.int32, [None, None])
+        target_placeholder = tf.placeholder(tf.float32, [None, 134])
+        batchSize_placeholder = tf.placeholder(tf.int32)
+        
         layers['input_placeholder'] = input_placeholder
         layers['target_placeholder'] = target_placeholder
 #         stepSize_placeholder = tf.placeholder(tf.int32)
@@ -109,28 +115,29 @@ with graph.as_default():
         voc_size = 1816
         embedding_size = 300
         with tf.variable_scope('embedding'):
-#         layers['embedding'] = tf.zeros([voc_size, embedding_size])
             layers['embedding'] = tf.get_variable(name='embedding', initializer=tf.random_uniform([voc_size, embedding_size], -1.0, 1.0), dtype=tf.float32)
         layers['input_embedding'] = tf.nn.embedding_lookup(layers['embedding'], input_placeholder)
-#         print(input_embedding)
+
         with tf.name_scope('lstm_layer1'):
             layers['lstm1'] = lstm_layer('lstm_layer1', layers['input_embedding'], embedding_size)
-#         print(lstm1)
+#         print(layers['lstm1'].outputs)
+            
         with tf.name_scope('reshape_layer1'):
-            layers['reshape1'] = reshape_layer('reshape_layer1', layers['lstm1'].outputs[:,-1,:], [1, 1, embedding_size, 1])
-#         print(reshape1.outputs)
+            layers['reshape1'] = reshape_layer('reshape_layer1', layers['lstm1'].outputs[:,-1,:], [batchSize_placeholder, 1, embedding_size, 1])
+#         print(layers['reshape1'])
+        
         with tf.name_scope('conv_layer1'):
             layers['conv1'] = conv_layer('conv_layer1', layers['reshape1'].outputs, [1, 50, 1, 1], tf.nn.relu)
-#         print(conv1.outputs)
+
         with tf.name_scope('reshape_layer2'):
-            layers['reshape2'] = reshape_layer('reshape_layer2', layers['conv1'].outputs, [1, 251])
-#         print(reshape2)
+            layers['reshape2'] = reshape_layer('reshape_layer2', layers['conv1'].outputs, [batchSize_placeholder, 251])
+
         with tf.name_scope('affine_layer1'):
             layers['affine1'] = affine_layer('affine_layer1', layers['reshape2'].outputs, [251, 128], tf.nn.relu)
-#         print(affine1)
+
         with tf.name_scope('affine_layer2'):
             layers['out'] = affine_layer('affine_layer2', layers['affine1'].outputs, [128, 134], tf.identity)
-#         print(out)
+
 
         for layer in layers.keys():
             try:
@@ -151,16 +158,19 @@ with graph.as_default():
             errs = 0.0
             accs = 0.0
             n = 0
-            for inputs_batch, targets_batch in zip(train_inputs, train_targets):
-                inputs = inputs_batch.reshape(1, -1)
-                targets = targets_batch.reshape(1, -1)
-                feed_dict = {input_placeholder: inputs, target_placeholder:targets}
-                _, err, acc = sess.run([optimizer, loss, accuracy], feed_dict=feed_dict)
+            for key in batched_keys.keys():
+#                 inputs = inputs_batch.reshape(1, -1)
+#                 targets = targets_batch.reshape(1, -1)
+                inputs = batched_inputs[key]
+                targets = batched_targets[key]
+                batchSize = batched_keys[key]
+#                 print(batchSize)
+#                 print(inputs.shape)
+#                 print(targets.shape)
+                feed_dict = {input_placeholder: inputs, target_placeholder:targets, batchSize_placeholder:batchSize}
+                _, err, acc, lookup = sess.run([optimizer, loss, accuracy, layers['input_embedding']], feed_dict=feed_dict)
+#                 print(lookup.shape)
                 n += 1
             errs = errs/n
             accs = accs/n
             print('#Training Epoch %d, ACC:%f, ERR:%f', i, accs, errs)
-
-    #     print(sess.run(input_embedding, feed_dict))
-    
-    
