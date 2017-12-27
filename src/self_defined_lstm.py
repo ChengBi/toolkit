f6672376c2c686985eb78b6d509d87d4eb971c64
Bi <cheng.bi@nuance.com>
2017-12-27 10:19:26 +0800

    Improve matrix multiple

--- src/self_defined_lstm.py
+++ src/self_defined_lstm.py
@@ -1,93 +1,112 @@
 import tensorflow as tf
 import numpy as np
 import pickle
-train_data = pickle.load(open('../word2vec/anouymous_train_data_200.npz', 'rb'))
-train_inputs = train_data['inputs']
-train_targets = train_data['targets']
-print(train_inputs['13_0'].shape)
-print(train_targets['13_0'].shape)
-padded_inputs = dict()
-padded_targets = dict()
-max_batch = 300
+#import matplotlib.pyplot as plt
+#%matplotlib inline
+
+max_batch = 200
 max_step = 40
 feature = 200
-for key in train_inputs.keys():
-    temp = train_inputs[key]
-    temp2 = train_targets[key]
-    if temp.shape[0] != max_batch and temp.shape[0] != 0:
-#         print(temp.shape)
-#         print(50-len(temp))
-#         print(np.zeros((50 - temp.shape[0], temp.shape[1], 200)).shape)
-        temp = np.concatenate((temp, np.zeros((max_batch - temp.shape[0], temp.shape[1], feature))), 0)
-        temp2 = np.concatenate((temp2, np.zeros((max_batch - temp2.shape[0], 134))), 0)
-#         print(temp.shape)
-        temp = np.concatenate((temp, np.zeros((max_batch, max_step - temp.shape[1], feature))), 1)
-        padded_inputs[key] = temp
-        padded_targets[key] = temp2
-    elif temp.shape[0] == max_batch and temp.shape[0] != 0:
-        temp = np.concatenate((temp, np.zeros((max_batch, max_step - temp.shape[1], feature))), 1)
-        padded_inputs[key] = temp
-        padded_targets[key] = train_targets[key]
-print(padded_inputs['13_1'].shape)
-print(padded_targets['13_1'].shape)
+
+def padding(filename):
+    train_data = pickle.load(open(filename, 'rb'))
+    train_inputs = train_data['inputs']
+    train_targets = train_data['targets']
+    print(train_inputs['13_0'].shape)
+    print(train_targets['13_0'].shape)
+    padded_inputs = dict()
+    padded_targets = dict()
+    for key in train_inputs.keys():
+        temp = train_inputs[key]
+        temp2 = train_targets[key]
+        if temp.shape[0] != max_batch and temp.shape[0] != 0:
+    #         print(temp.shape)
+    #         print(50-len(temp))
+    #         print(np.zeros((50 - temp.shape[0], temp.shape[1], 200)).shape)
+            temp = np.concatenate((temp, np.zeros((max_batch - temp.shape[0], temp.shape[1], feature))), 0)
+            temp2 = np.concatenate((temp2, np.zeros((max_batch - temp2.shape[0], 134))), 0)
+    #         print(temp.shape)
+            temp = np.concatenate((temp, np.zeros((max_batch, max_step - temp.shape[1], feature))), 1)
+            padded_inputs[key] = temp
+            padded_targets[key] = temp2
+        elif temp.shape[0] == max_batch and temp.shape[0] != 0:
+            temp = np.concatenate((temp, np.zeros((max_batch, max_step - temp.shape[1], feature))), 1)
+            padded_inputs[key] = temp
+            padded_targets[key] = train_targets[key]
+    print(padded_inputs['13_1'].shape)
+    print(padded_targets['13_1'].shape)
+    return padded_inputs, padded_targets
+    
+# padded_inputs, padded_targets = padding('../word2vec/anouymous_train_data_200.npz')
+# padded_test_inputs, padded_test_targets  = padding('../word2vec/anouymous_valid_data_200.npz')
+
+padded_inputs, padded_targets = pickle.load(open('../word2vec/anouymous_padded_train_data.npz', 'rb')).values()
+padded_test_inputs, padded_test_targets  = pickle.load(open('../word2vec/anouymous_padded_valid_data.npz', 'rb')).values()
+
+
+
+padded_inputs = np.reshape(padded_inputs, [670, 200, 20, 200])
+padded_targets = np.reshape(padded_targets, [670, 200, 134])
+padded_test_inputs = np.reshape(padded_test_inputs, [67, 200, 20, 200])
+padded_test_targets = np.reshape(padded_test_targets, [67, 200, 134])
 
 def init(shape):
     return tf.Variable(tf.truncated_normal(shape), tf.float32)
 
 def lstm_layer(inputs, batch_size, step_size):
-    shape = inputs.shape.as_list()
-    batch_size = shape[0]
-    step_size = shape[1]
-    n_units = shape[2]
-#     print(batch_size)
-#     print(step_size)
+    with tf.name_scope('lstm'):
+        shape = inputs.shape.as_list()
+        batch_size = shape[0]
+        step_size = shape[1]
+        n_units = shape[2]
+    #     print(batch_size)
+    #     print(step_size)
 
-    wf = init([n_units, n_units])
-    bf = init([n_units])
+        wf = init([n_units, n_units])
+        bf = init([n_units])
 
-    wi = init([n_units, n_units])
-    bi = init([n_units])
+        wi = init([n_units, n_units])
+        bi = init([n_units])
 
-    wc = init([n_units, n_units])
-    bc = init([n_units])
+        wc = init([n_units, n_units])
+        bc = init([n_units])
 
-    wo = init([n_units, n_units])
-    bo = init([n_units])
+        wo = init([n_units, n_units])
+        bo = init([n_units])
 
-    ht = init([batch_size, n_units])
-    ct = init([1, n_units])
+        ht = init([batch_size, n_units])
+        ct = init([1, n_units])
 
-    ct_tiled = tf.tile(ct, [batch_size, 1])
+        ct_tiled = tf.tile(ct, [batch_size, 1])
+
+#         W = tf.transpose(tf.tile(tf.concat([wf, wi, wc, wo], 0), [1, 2]), [1, 0])
+#         B = tf.concat([bf, bi, bc, bo], 0)
+        W = tf.concat([wf, wi, wc, wo], 0)
+        B = tf.concat([bf, bi, bc, bo], 0)
+
+#         temp = (np.dot(W, x.T) + np.dot(W, h.T)).T + B
+
+#         t1, t2, t3, t4 = np.split(temp, indices_or_sections=4 ,axis=1)
+
+        for i in range(step_size):
+            xt = inputs[:, i, :]
+#             X = tf.concat([ht, xt], 1)
+#             V = tf.matmul(X, W) + B
+            V = tf.transpose(tf.matmul(W, tf.transpose(xt, [1, 0])) + tf.matmul(W, tf.transpose(ht, [1,0])), [1,0]) + B
     
-    W = tf.transpose(tf.tile(tf.concat([wf, wi, wc, wo], 0), [1, 2]), [1, 0])
-    B = tf.concat([bf, bi, bc, bo], 0)
-    
-        
-    for i in range(step_size):
-        xt = inputs[:, i, :]
-#         ft = tf.sigmoid(tf.matmul(ht, wf) + tf.matmul(xt, wf) + bf)
-#         it = tf.sigmoid(tf.matmul(ht, wi) + tf.matmul(xt, wi) + bi)
-#         c = tf.tanh(tf.matmul(ht, wc) + tf.matmul(xt, wc) + bc)
-#         ot = tf.sigmoid(tf.matmul(ht, wo) + tf.matmul(xt, wo) + bo)
-        X = tf.concat([ht, xt], 1)
-#         print(ht.shape)
-#         print(xt.shape)
-#         print(X.shape)
-        V = tf.sigmoid(tf.matmul(X, W) + B)
-#         print(W.shape)
-#         print(B.shape)
-        ft, it, c, ot = tf.split(V, num_or_size_splits=4, axis=1)
-#         print(ft.shape)
-#         print(it.shape)
-#         print(c.shape)
-#         print(ot.shape)
-        
-        ct = tf.reshape(tf.reduce_mean(ft * ct_tiled + it * c, 0), [1, n_units])
-        ht = ot * tf.tile(tf.tanh(ct), [batch_size, 1])
+            ft, it, c, ot = tf.split(V, num_or_size_splits=4, axis=1)
+            ft = tf.sigmoid(ft)
+            it = tf.sigmoid(it)
+            c = tf.tanh(c)
+            ot = tf.sigmoid(ot)
+            ct = tf.reshape(tf.reduce_mean(ft * ct_tiled + it * c, 0), [1, n_units])
+            ht = ot * tf.tile(tf.tanh(ct), [batch_size, 1])
         
     return ht, ct
 
-
+max_batch = 200
+max_step = 20
+feature = 200
 graph = tf.Graph()
 with graph.as_default():
     
@@ -108,43 +127,59 @@ with graph.as_default():
 #     print(cells)
     outputs, states = lstm_layer(inputs_placeholder, batch_size, step_size)
     
-    w1 = init([200, 300])
-    b1 = init([300])
+    w1 = init([200, 128])
+    b1 = init([128])
     
-    w2 = init([300, 300])
-    b2 = init([300])
+#     w2 = init([128, 128])
+#     b2 = init([128])
     
-    w3 = init([300, 134])
+    w3 = init([128, 134])
     b3 = init([134])
     
     o1 = tf.nn.relu(tf.add(tf.matmul(outputs, w1), b1))
-    o2 = tf.nn.relu(tf.add(tf.matmul(o1, w2), b2))
-    o3 = tf.nn.relu(tf.add(tf.matmul(o2, w3), b3))
+#     o2 = tf.nn.relu(tf.add(tf.matmul(o1, w2), b2))
+    o3 = tf.nn.relu(tf.add(tf.matmul(o1, w3), b3))
     
     loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=o3, labels=targets_placeholder))
     sum_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=o3, labels=targets_placeholder))
-    optimizer = tf.train.AdamOptimizer().minimize(loss)
+    optimizer = tf.train.AdamOptimizer(1e-2).minimize(loss)
     accuracy = tf.reduce_sum(tf.cast(tf.equal(tf.argmax(o3, 1), tf.argmax(targets_placeholder, 1)), tf.float32))
     
     sess = tf.Session()
+    
+    merged = tf.summary.merge_all()
+    writer = tf.summary.FileWriter('../outputs/', sess.graph)
     sess.run(tf.global_variables_initializer())
     
-    keys = padded_inputs.keys()
-    
+#     keys = padded_inputs.keys()
+#     test_keys = padded_test_inputs.keys()
+    print('TRAINING PROCESS START...')
     for i in range(100):
         accs = 0.0
         errs = 0.0
         n = 0
-        for key in keys:
-            batch_inputs = padded_inputs[key]
-            batch_targets = padded_targets[key]
-            batch_size = len(train_inputs)
-#             step_size = key.split('_')[0]
-#             print(batch_inputs.shape)
-#             print(batch_targets.shape)
+        print('#EPOCH => %d'%(i))
+        for batch_inputs, batch_targets in zip(padded_inputs[:], padded_targets[:]):
+            batch_size = len(batch_inputs)
             n += batch_size
             feed_dict = {inputs_placeholder:batch_inputs, targets_placeholder:batch_targets}#, batch_size:batch_size, step_size:step_size}
             _, acc, err = sess.run([optimizer, accuracy, sum_loss], feed_dict=feed_dict)
             accs += acc
             errs += err
-        print('#Trainig Epoch: %d, ACC:%f ERR:%f'%(i, accs/n, errs/n))
+#         writer.add_summary(m, i)
+        print('#Trainig Epoch: %d, ACC:%f, ERR:%f, N:%d'%(i, accs/n, errs/n, n))
+        if i % 5 == 0:
+            accs = 0.0
+            errs = 0.0
+            n = 0
+            for batch_inputs, batch_targets in zip(padded_test_inputs[:], padded_test_targets[:]):
+                batch_size = len(batch_inputs)
+                n += batch_size
+                feed_dict = {inputs_placeholder:batch_inputs, targets_placeholder:batch_targets}#, batch_size:batch_size, step_size:step_size}
+                acc, err = sess.run([accuracy, sum_loss], feed_dict=feed_dict)
+                accs += acc
+                errs += err
+            print('------------------------------------------------------------------')
+            print('#Testing Epoch: %d, ACC:%f, ERR:%f, N:%d'%(i, accs/n, errs/n, n))
+            print('------------------------------------------------------------------')
+            
